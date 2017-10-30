84a6e969f0bdf44d8720a735d6f816f86e57939e
Alex Soto <asotobu@gmail.com>
2017-10-30 12:06:21 +0100

    Adds pom.xml

--- src/test/java/org/music/RestAPITest.java
+++ src/test/java/org/music/RestAPITest.java
@@ -3,17 +3,38 @@ package org.music;
 import org.apache.http.client.fluent.Content;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.client.utils.URIBuilder;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import org.junit.runner.RunWith;
+import org.music.boundary.Composers;
+import org.music.boundary.ComposersResource;
+import org.music.entity.Composer;
+import org.wildfly.swarm.jaxrs.JAXRSArchive;
 
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.CoreMatchers.is;
 
+@RunWith(Arquillian.class)
+//@DefaultDeployment(main = ComposersMain.class, testable = false, type = DefaultDeployment.Type.JAR)
 public class RestAPITest {
+
+    @Deployment(testable = false)
+    public static Archive createDeployment() throws Exception {
+        JAXRSArchive deployment = ShrinkWrap.create(JAXRSArchive.class);
+        deployment.addClasses(ComposersResource.class, Composers.class);
+        deployment.addPackage(Composer.class.getPackage());
+        deployment.addAllDependencies();
+
+        return deployment;
+    }
 
     @Test
     public void should_find_composer() throws IOException, URISyntaxException {
